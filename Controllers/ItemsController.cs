789efa99ab60e044a87ddf5c8fa2687a8e8536ce
rudev2021 <87792059+rudev2021@users.noreply.github.com>
2021-07-23 01:32:11 -0700

    V1.1 - added api routes for #1, #2 Update: all working

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -18,11 +18,52 @@ namespace eScholarshipAPI_Client.Controllers
             _consumer = consumer;
         }
 
-        [HttpGet]
+        [HttpGet]   // url: ~/api/items/
         public async Task<IActionResult> Get()
         {
             var items = await _consumer.GetPublishedItems();
             return Ok(items);
         }
+
+        [HttpGet]
+        [Route("No1")]      // url: ~/api/items/No1/
+        public async Task<IActionResult> GetNo1()
+        {
+            var items = await _consumer.GetPublishedItems();
+            // #1: Find all of the items published between April 2, 2020 and June 30, 2021, and the units they are associated with.
+            DateTime dStart = new DateTime(2020, 4, 2);
+            DateTime dEnd = new DateTime(2021, 7, 1);
+            var items1 = ItemConsumer.GetItemsWithinPeriod(items, dStart, dEnd);  // list1: #1  (count: 76)
+            return Ok(items1);
+        }
+
+        [HttpGet]
+        [Route("No2")]      // url: ~/api/items/No2/
+        public async Task<IActionResult> GetNo2()
+        {
+            var items = await _consumer.GetPublishedItems();
+            // #1: Find all of the items published between April 2, 2020 and June 30, 2021, and the units they are associated with.
+            DateTime dStart = new DateTime(2020, 4, 2);
+            DateTime dEnd = new DateTime(2021, 7, 1);
+            var items1 = ItemConsumer.GetItemsWithinPeriod(items, dStart, dEnd);  // list1: #1  (count: 76)
+            // #2: present a count of the number of items per unit, published during that time frame.
+            var items2 = ItemConsumer.GetItemsPerUnit(items1);
+            return Ok(items2);
+        }
+
+        [HttpGet]
+        [Route("File")]      // url: ~/api/items/file/
+        public async Task<IActionResult> GetFile()
+        {
+            var items = await _consumer.GetItemsFromFile();
+            // #1: Find all of the items published between April 2, 2020 and June 30, 2021, and the units they are associated with.
+            DateTime dStart = new DateTime(2020, 4, 2);
+            DateTime dEnd = new DateTime(2021, 7, 1);
+            var items1 = ItemConsumer.GetItemsWithinPeriod(items, dStart, dEnd);  // list1: #1  (count: 76)
+            // #2: present a count of the number of items per unit, published during that time frame.
+            var items2 = ItemConsumer.GetItemsPerUnit(items1);
+            return Ok(items2);
+        }
+
     }
 }
--- Models/Model.cs
+++ Models/Model.cs
@@ -20,7 +20,7 @@ namespace eScholarshipAPI_Client.Models
         }
 
 
-        public List<Item> GetItemsWithinPeriod(List<Item> list, DateTime dStart, DateTime dEnd) 
+        public static List<Item> GetItemsWithinPeriod(List<Item> list, DateTime dStart, DateTime dEnd) 
         {
             // #1: Find all of the items published between April 2, 2020 and June 30, 2021, and the units they are associated with.
             //DateTime dStart = new DateTime(2020, 4, 2);
@@ -36,13 +36,11 @@ namespace eScholarshipAPI_Client.Models
             return list1;   
         }
 
-        public List<ItemsPerUnit> GetItemsPerUnitWithinPeriod(List<Item> list, DateTime dStart, DateTime dEnd)
+        public static List<ItemsPerUnit> GetItemsPerUnit(List<Item> list)
         {
-            var list1 = GetItemsWithinPeriod(list, dStart, dEnd);
-
             // #2: present a count of the number of items per unit, published during that time frame.
             List<Unit> units = new List<Unit>();
-            foreach (var lst in list1)
+            foreach (var lst in list)
             {
                 var tmp = (from c in lst.units
                            select c);
@@ -59,7 +57,7 @@ namespace eScholarshipAPI_Client.Models
             foreach (var u in unitList)
             {
                 ItemsPerUnit items = new ItemsPerUnit();
-                var lst = (from c in list1
+                var lst = (from c in list
                            where
                             c.units.Any(m => m.id == u.id)
                            select c).ToList();
@@ -70,6 +68,18 @@ namespace eScholarshipAPI_Client.Models
 
             return itemUnitList;
         }
+
+        public async Task<List<Item>> GetItemsFromFile()
+        {
+            // test deserailize with local file - okay!
+            using (StreamReader file = File.OpenText(@"Models\Data.json"))
+            {
+                var jsonString = await file.ReadToEndAsync();
+                ItemRootobject itemRootObj = (ItemRootobject)JsonSerializer.Deserialize(jsonString, typeof(ItemRootobject));
+                var list = itemRootObj.data.items.item.ToList();
+                return list;
+            }
+        } 
 
         public async Task<List<Item>> GetPublishedItems()
         {
@@ -99,61 +109,12 @@ namespace eScholarshipAPI_Client.Models
 
             try
             {
-                // test deserailize - okay!
-                using (StreamReader file = File.OpenText(@"Models\Data.json"))
-                {
-                    ItemRootobject itemRootObj = (ItemRootobject)JsonSerializer.Deserialize(file.ReadToEnd(), typeof(ItemRootobject));
-                    var list = itemRootObj.data.items.item.ToList();
-               
-                    // #1: Find all of the items published between April 2, 2020 and June 30, 2021, and the units they are associated with.
-                    DateTime dStart = new DateTime(2020, 4, 2);
-                    DateTime dEnd = new DateTime(2021, 7, 1);  
-
-
-                    var list1 = (from c in list where 
-                                 (DateTime.Parse(c.published) >= dStart
-                                 && DateTime.Parse(c.published) < dEnd) 
-                                 select c).ToList();
-                    // list1: #1  (count: 76)
-
-
-                    // #2: present a count of the number of items per unit, published during that time frame.
-                    List<Unit> units = new List<Unit>();
-                    foreach (var lst in list1)
-                    {
-                        var tmp = (from c in lst.units
-                                   select c);
-                        units.AddRange(tmp);
-                    }
-
-                    // distinct unit id's  (count: 19)
-                    var unitList = units.GroupBy(x => x.id)
-                                  .Select(g => g.First())
-                                  .ToList();
-
-                    List<ItemsPerUnit> itemUnitList = new List<ItemsPerUnit>();
-
-                    foreach(var u in unitList)
-                    {
-                        ItemsPerUnit items = new ItemsPerUnit();
-                        var lst = (from c in list1
-                                   where
-                                    c.units.Any(m => m.id == u.id)
-                                   select c).ToList();
-                        items.unitID = u.id;
-                        items.items = lst;
-                        itemUnitList.Add(items);
-                    }
-
-                    // itemUnitList: #2
-
-                }
-
+                // connect to API
                 var response = await _client.SendQueryAsync<JObject>(query);
       
                 if (response.Data != null)
                 {
-                    string jsonString = response.Data.ToString();
+                    string jsonString = response.Data.ToString();   // verify json response
                     var responseItems = JsonSerializer.Deserialize<Data>(jsonString);
                     return responseItems.items.item.ToList();
                 }
@@ -174,8 +135,8 @@ namespace eScholarshipAPI_Client.Models
 
     public class ItemsPerUnit
     {
-        public string unitID;
-        public List<Item> items;
+        public string unitID { get; set; }
+        public List<Item> items { get; set; }
         public int count { 
             get {
                 if (items == null)
